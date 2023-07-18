0d1aa74ea8fa97c52955e9dd385f4a218231a4d7
Stephen Flee <steveflee@gmail.com>
2023-07-18 11:47:16 -0700

    Bump

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_kinesis_firehose_delivery_stream" "covid_data_stream" {
 
   extended_s3_configuration {
     role_arn        = aws_iam_role.firehose_role.arn
-    bucket_arn      = "arn:aws:s3:::thelatestlead"
+    bucket_arn      = var.s3_bucket_arn
     prefix          = "${var.environment}/data/covid_data_stream"
     buffer_interval = 60
     cloudwatch_logging_options {
@@ -33,4 +33,51 @@ data "aws_iam_policy_document" "firehose_assume_role" {
 resource "aws_iam_role" "firehose_role" {
   name               = "firehose_test_role"
   assume_role_policy = data.aws_iam_policy_document.firehose_assume_role.json
+
+  inline_policy {
+    name = "s3_write"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "s3:AbortMultipartUpload",
+            "s3:GetBucketLocation",
+            "s3:GetObject",
+            "s3:ListBucket",
+            "s3:ListBucketMultipartUploads",
+            "s3:PutObject"
+          ]
+          Resource = [
+            var.s3_bucket_arn,
+            "${var.s3_bucket_arn}/*"
+          ]
+        },
+      ]
+    })
+  }
+
+  inline_policy {
+    name = "kinesis_read"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          Action = [
+            "kinesis:DescribeStream",
+            "kinesis:GetShardIterator",
+            "kinesis:GetRecords",
+            "kinesis:ListShards"
+          ]
+          Resource = [
+            covid_data_stream.arn
+          ]
+        },
+      ]
+    })
+  }
 }
--- vars.tf
+++ vars.tf
@@ -2,3 +2,8 @@ variable "environment" {
   type    = string
   default = "production"
 }
+
+variable "s3_bucket_arn" {
+  type    = string
+  default = "arn:aws:s3:::thelatestlead"
+}
