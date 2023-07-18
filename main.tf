c508316945162b39ac357d4e7a7c9cb1dca75013
Stephen Flee <steveflee@gmail.com>
2023-07-18 11:08:43 -0700

    Firehose data stream added

---
+++ main.tf
@@ -0,0 +1,29 @@
+
+resource "aws_kinesis_firehose_delivery_stream" "covid_data_stream" {
+  name        = "covid_data_stream"
+  destination = "extended_s3"
+
+  extended_s3_configuration {
+    role_arn   = aws_iam_role.firehose_role.arn
+    bucket_arn = "arn:aws:s3:::thelatestlead"
+    prefix     = "production/data/covid_data_stream"
+  }
+}
+
+data "aws_iam_policy_document" "firehose_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["firehose.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "firehose_role" {
+  name               = "firehose_test_role"
+  assume_role_policy = data.aws_iam_policy_document.firehose_assume_role.json
+}
---
+++ providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "thelatestlead"
+    key    = "production/infastructure"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "thelatestlead"
+}
