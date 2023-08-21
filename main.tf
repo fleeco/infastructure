de48a3c0e23dcb855378ca724725807ddb9f0c49
Stephen Flee <steveflee@gmail.com>
2023-08-21 10:41:47 -0700

    Adds mo

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+module "management-us-west-2" {
+  source             = "./modules/eks"
+  region             = "us-west-2"
+  environment        = terraform.workspace
+  cluster_name       = "${terraform.workspace}-management"
+  iam_cluster_admin  = aws_iam_role.eks_cluster_admin
+  iam_cluster_role   = aws_iam_role.eks_cluster_role
+  iam_node_role      = aws_iam_role.eks_node_role
+  iam_karpenter_role = aws_iam_role.karpenter
+  iam_ebs_csi_role   = aws_iam_role.ebs_csi
+}
+
 
 resource "aws_kinesis_firehose_delivery_stream" "covid_data_stream" {
   name        = "${var.environment}_covid_data_stream"
