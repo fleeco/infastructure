339908375e0096de3a2f2e2243fe8369c5fd631f
Stephen Flee <steveflee@gmail.com>
2023-08-21 10:40:36 -0700

    Adds some mo

--- vars.tf
+++ vars.tf
@@ -7,3 +7,23 @@ variable "s3_bucket_arn" {
   type    = string
   default = "arn:aws:s3:::thelatestlead"
 }
+
+# data "aws_ssm_parameter" "github_ssh_key" {
+#   name = "/env/p0/kubekilla/ssh"
+# }
+
+locals {
+  oidc_providers = {
+    "management-us-east-1" : module.management-us-west-2.openid_connect_provider,
+    # "delivery-us-east-1" : module.delivery-us-east-1.openid_connect_provider,
+    # "delivery-us-west-2" : module.delivery-us-west-2.openid_connect_provider
+    # "delivery-eu-west-1" : module.delivery-eu-west-1.openid_connect_provider,
+    # "delivery-ap-southeast-2" : module.delivery-ap-southeast-2.openid_connect_provider
+  }
+  clusters = {
+    # "del-use-1" : module.delivery-us-east-1.eks_cluster,
+    # "del-usw-2" : module.delivery-us-west-2.eks_cluster,
+    # "del-euw-1" : module.delivery-eu-west-1.eks_cluster,
+    # "del-aps-2" : module.delivery-ap-southeast-2.eks_cluster
+  }
+}
